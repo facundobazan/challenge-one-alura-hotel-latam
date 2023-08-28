360b1828aeb19bd5ec7c8c1fbecf9e69f811a418
Facundo Esteban Bazán <facundobazan@icloud.com>
2023-08-28 01:50:27 -0300

    Test funcionamiento de javafx

--- src/main/java/ar/com/facundobazan/hotel_alura/views/BuscarHuesped.java
+++ src/main/java/ar/com/facundobazan/hotel_alura/views/BuscarHuesped.java
@@ -2,6 +2,7 @@ package ar.com.facundobazan.hotel_alura.views;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,13 +10,14 @@ public class BuscarHuesped extends Application {
 
     public static void main(String[] args) {
 
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader fxmlLoader = FXMLLoader.load(BuscarHuesped.class.getResource("./views/buscar-huesped.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Parent root = FXMLLoader.load(BuscarHuesped.class.getResource("/views/buscar-huesped.fxml"));
+        Scene scene = new Scene(root);
 
         stage.setTitle("Buscar Huesped");
         stage.setResizable(false);
